43e72f96ada11827ca2de7fcbb15995a67326928
ykmtd <ykmtd@example.com>
2015-11-17 23:56:17 +0900

    longer initial reset for Nexys4

--- fpga/nexys4/rtl/clock/clock.v
+++ fpga/nexys4/rtl/clock/clock.v
@@ -42,8 +42,8 @@ module RSTGEN(CLK, RST_X_I, RST_X_O);
     input  CLK, RST_X_I;
     output RST_X_O;
 
-    reg [7:0] cnt;
-    assign RST_X_O = cnt[7];
+    reg [23:0] cnt;
+    assign RST_X_O = cnt[23];
 
     always @(posedge CLK or negedge RST_X_I) begin
         if      (!RST_X_I) cnt <= 0;
